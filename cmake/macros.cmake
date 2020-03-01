93b277b0936d2c5c68b58dd7d506a1e6fae23da7
Weiming-Hu <weiming@psu.edu>
2020-03-01 10:12:16 -0500

    C++ with SSC

--- cmake/macros.cmake
+++ cmake/macros.cmake
@@ -9,16 +9,109 @@
 # This file includes some macros for the project
 #
 
-# Check whether SSC source files have been downloaded
+# This macro takes care of creating a target of AnEnIO
+macro(add_AnEnIO)
+
+    if(AnEnIO_ROOT_DIR)
+        set(AnEnIO_INCLUDE_DIR "${AnEnIO_ROOT_DIR}/include")
+        set(AnEnIO_OBJECT "${AnEnIO_ROOT_DIR}/lib/cmake/AnEnIOTargets.cmake")
+        set(AnEnIO_LIB "${AnEnIO_ROOT_DIR}/lib/libAnEnIO.so")
+    else(AnEnIO_ROOT_DIR)
+
+        # Check whether it is under ~/packages/release
+        # This is just my personal habit
+        #
+        set(AnEnIO_INCLUDE_DIR "$ENV{HOME}/packages/release/include")
+        set(AnEnIO_OBJECT "$ENV{HOME}/packages/release/lib/cmake/AnEnIOTargets.cmake")
+        set(AnEnIO_LIB "$ENV{HOME}/packages/release/lib/libAnEnIO.so")
+        set(AnEn_LIB "$ENV{HOME}/packages/release/lib/libAnEn.so")
+
+        if(NOT EXISTS ${AnEnIO_LIB})
+            message(FATAL_ERROR "Please specify the installation root directory of AnEnIO (AnEnIO_ROOT_DIR)")
+        endif(NOT EXISTS ${AnEnIO_LIB})
+
+    endif(AnEnIO_ROOT_DIR)
+
+    add_library(AnEnIO SHARED IMPORTED)
+
+    set_target_properties(AnEnIO PROPERTIES
+        INTERFACE_INCLUDE_DIRECTORIES ${AnEnIO_INCLUDE_DIR}
+        INTERFACE_LINK_LIBRARIES ${AnEn_LIB}
+        IMPORTED_LOCATION ${AnEnIO_LIB})
+
+endmacro(add_AnEnIO)
+
+
+# This macro takes care of creating a target of ssc
+macro(add_ssc)
+
+    # Check whether SSC has been downloaded already
+    if(BUILD_SSC)
+        check_ssc_download()
+
+        # Add SSC to our current project
+        add_subdirectory(${CMAKE_CURRENT_BINARY_DIR}/ssc)
+
+        # This property was not set in the sub project. Take care of setting
+        # this here to save troubles elsewhere.
+        #
+        set_target_properties(ssc PROPERTIES 
+            INTERFACE_INCLUDE_DIRECTORIES "${CMAKE_CURRENT_BINARY_DIR}/ssc/ssc")
+
+    else(BUILD_SSC)
+        add_ssc_library()
+    endif(BUILD_SSC)
+
+endmacro(add_ssc)
+
+
+# Add a prebuilt version of SSC. This only supports Linux platforms.
+#
+# You don't need to call this function explicitly. Use add_ssc.
+#
+macro(add_ssc_library)
+
+    # These are required files
+    set(SSC_INCLUDE_FILE "${CMAKE_SOURCE_DIR}/ssc/sscapi.h")
+    set(SSC_LIB_FILE "${CMAKE_SOURCE_DIR}/ssc/ssc.so")
+
+    message(STATUS "Use pre-built SSC library")
+
+    # Add target
+    add_library(ssc SHARED IMPORTED)
+
+    set_target_properties(ssc PROPERTIES
+        INTERFACE_INCLUDE_DIRECTORIES "${CMAKE_CURRENT_SOURCE_DIR}/ssc"
+        IMPORTED_LOCATION ${SSC_LIB_FILE}
+        PUBLIC_HEADER ${SSC_INCLUDE_FILE})
+
+    target_link_libraries(ssc INTERFACE dl)
+
+endmacro(add_ssc_library)
+
+
+# Check whether SSC source files have been downloaded. If not, download the source
+# files to the current binary source directory
+#
+# You don't need to call this function explicitly. Use add_ssc.
+#
 macro(check_ssc_download)
 
     if(NOT EXISTS ${CMAKE_CURRENT_BINARY_DIR}/ssc/CMakeLists.txt)
         find_package(Git 1.9 REQUIRED QUIET)
-        message(STATUS "Downloading source files for SSC ...")
+
+        message(STATUS "Downloading source files for SSC")
+        message(STATUS "The SSC github repository is roughly 1.5 GB")
+        message(STATUS "This might take a while depending on your internet connection")
+
         execute_process(COMMAND ${GIT_EXECUTABLE} "clone"
             "https://github.com/NREL/ssc.git"
             "${CMAKE_CURRENT_BINARY_DIR}/ssc")
+
         message(STATUS "SSC source files downloaded at ${CMAKE_CURRENT_BINARY_DIR}/ssc")
+
+    else(NOT EXISTS ${CMAKE_CURRENT_BINARY_DIR}/ssc/CMakeLists.txt)
+        message(STATUS "SSC source files have been downloaded")
     endif(NOT EXISTS ${CMAKE_CURRENT_BINARY_DIR}/ssc/CMakeLists.txt)
 
 endmacro(check_ssc_download)
--- evergreen.cpp
+++ evergreen.cpp
@@ -1,4 +1,5 @@
 /* 
+ *
  * File:   evergreen.cpp
  * Author: Weiming Hu <weiming@psu.edu>
  *
@@ -7,11 +8,140 @@
 
 /** @file */
 
+#include <string>
 #include <iostream>
+#include <stdexcept>
+#include <algorithm>
+#include <iterator>
+
+#include "sscapi.h"
+
+#include "Times.h"
 
 using namespace std;
 
-void main() {
-    cout << "evergreen -- our pursuit for a more sustainable society" << endl;
+static const int _NUM_TIMES = 8760;
+
+
+void printVersion() {
+    
+    int version = ssc_version();
+
+    const char *build = ssc_build_info();
+    cout << "SSC version " << version << ": " << build << endl;
+    
     return;
+}
+
+void test_pvwattsv5() {
+    cout << "Test module pvwattsv5" << endl;
+    
+    /*
+     * Prepare array data
+     */
+    ssc_number_t dn[_NUM_TIMES], df[_NUM_TIMES], tdry[_NUM_TIMES], wspd[_NUM_TIMES];
+    
+    fill(dn, dn + _NUM_TIMES, 3.5);
+    fill(df, df + _NUM_TIMES, 23);
+    fill(tdry, tdry + _NUM_TIMES, 27);
+    fill(wspd, wspd + _NUM_TIMES, 4);
+    
+    /*
+     * Create weather data table
+     */
+    ssc_data_t weather_table = ssc_data_create();
+    
+    // Assign data arrays to the table
+    ssc_data_set_array(weather_table, "dn", dn, _NUM_TIMES);
+    ssc_data_set_array(weather_table, "df", df, _NUM_TIMES);
+    ssc_data_set_array(weather_table, "tdry", tdry, _NUM_TIMES);
+    ssc_data_set_array(weather_table, "wspd", wspd, _NUM_TIMES);
+    
+    ssc_data_set_number(weather_table, "lat", 40);
+    ssc_data_set_number(weather_table, "lon", 70);
+    ssc_data_set_number(weather_table, "tz", -8);
+
+    
+    /*
+     * Create the data container
+     */
+    ssc_data_t data_container = ssc_data_create();
+    
+    ssc_data_set_number(data_container, "system_capacity", 1);
+    ssc_data_set_number(data_container, "losses", 0);
+    ssc_data_set_number(data_container, "array_type", 0);
+    ssc_data_set_number(data_container, "tilt", 20);
+    ssc_data_set_number(data_container, "azimuth", 180);
+    ssc_data_set_number(data_container, "adjust:constant", 0);
+
+    // Weather table is the different one from others
+    ssc_data_set_table(data_container, "solar_resource_data", weather_table);
+    
+    // Free the table after it is assigned
+    ssc_data_free(weather_table);
+    
+    
+    /*
+     * Run simulation
+     */
+    ssc_module_t module = ssc_module_create("pvwattsv5");
+    
+    // Return error if the module was not created successfully
+    if (module == NULL) {
+        ssc_data_free(data_container);
+        throw runtime_error("could not create pvwattsv5 module");
+    }
+    
+    // Run module
+    if (ssc_module_exec(module, data_container) == 0) {
+        ssc_module_free(module);
+        ssc_data_free(data_container);
+        throw runtime_error("pvwattsv5 simulation failed");
+    }
+    
+    
+    /*
+     * Retrieve results
+     */
+    int len = 0;
+    ssc_number_t *ac = ssc_data_get_array(data_container, "ac", &len);
+    
+    if (ac == NULL) {
+        throw runtime_error("variable 'ac' not found");
+    } else {
+        cout << "ac: ";
+        
+        int count = 0;
+        
+        for (int i = 0; i < len; ++i) {
+            cout << ac[i] << ',';
+            count++;
+            
+            if (count == 24) {
+                cout << endl;
+                count = 0;
+            }
+            
+        }
+    }
+    
+    
+    /*
+     * Housekeeping
+     */
+    ssc_module_free(module);
+    ssc_data_free(data_container);
+    
+    return;
+}
+
+
+int main() {
+    cout << "evergreen -- our pursuit for a more sustainable future" << endl;
+    
+    test_pvwattsv5();
+
+    Time time(100);
+    
+    return 0;
 }
